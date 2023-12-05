5d8582503b52e2746c46b22a6169f9d8cb02280d
Pavel <kedovpa@gmail.com>
2023-12-05 18:06:40 +0600

    Fixed Build App

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export { Content } from './components/content';
-export { Footer } from './components/footer';
-export { Sidebar } from './components/sidebar';
-export { AppRoutes } from "./AppRoutes";
-export { Header } from "./pages/Header/Header";
